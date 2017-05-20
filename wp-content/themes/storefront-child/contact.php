4cd4839b7442cf6fb778a015a600480828a54f7e
Eddie <eddie.jostell@gmail.com>
2017-05-20 22:14:54 +0200

    made my 5 pages.php

---
+++ wp-content/themes/storefront-child/contact.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Tempalte name: Contact Template
+
+*/
+get_header(); ?>
+<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+        <section class="contact-header">
+        	<h1>Didnt find the right tool for the job, or just wanted to say how aswesome the site is?</h1>
+        	<h1>Send us a message, Peace!</h1>
+        </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
+?>
