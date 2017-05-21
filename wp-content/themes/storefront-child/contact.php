809c3857f8a71134e4f5028d9faab134998e1ee3
Eddie <eddie.jostell@gmail.com>
2017-05-21 23:14:58 +0200

    contact template, needs some work

--- wp-content/themes/storefront-child/contact.php
+++ wp-content/themes/storefront-child/contact.php
@@ -1,17 +1,79 @@
 <?php
 /*
-Tempalte name: Contact Template
+Template name: Contact Template
 
 */
 get_header(); ?>
-<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-        <section class="contact-header">
-        	<h1>Didnt find the right tool for the job, or just wanted to say how aswesome the site is?</h1>
-        	<h1>Send us a message, Peace!</h1>
-        </section>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary">
+	<div id="content" role="main">
+		<font color="#FF0000">
+			<?php
+			if(isset($_POST['submit']))
+			{
+				$flag=1;
+				if($_POST['yourname']=='')
+				{
+					$flag=0;
+					echo "Please Enter Your Name<br>";
+				}
+				else if(!preg_match('/[a-zA-Z_x7f-xff][a-zA-Z0-9_x7f-xff]*/',$_POST['yourname']))
+				{
+					$flag=0;
+					echo "Please Enter Valid Name<br>";
+				}
+				if($_POST['email']=='')
+				{
+					$flag=0;
+					echo "Please Enter E-mail<br>";
+				}
+				else if(!eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $_POST['email']))
+				{
+					$flag=0;
+					echo "Please Enter Valid E-Mail<br>";
+				}
+				if($_POST['subject']=='')
+				{
+					$flag=0;
+					echo "Please Enter Subject<br>";
+				}
+				if($_POST['message']=='')
+				{
+					$flag=0;
+					echo "Please Enter Message";
+				}
+				if ( empty($_POST) )
+				{
+					print 'Sorry, your nonce did not verify.';
+					exit;
+				}
+				else
+				{
+					if($flag==1)
+					{
+						wp_mail(get_option("admin_email"),trim($_POST[yourname])." sent you a message from ".get_option("blogname"),stripslashes(trim($_POST[message])),"From: ".trim($_POST[yourname])." <".trim($_POST[email]).">rnReply-To:".trim($_POST[email]));
+						echo "Mail Successfully Sent";
+					}
+				}
+			}
+			?>
+		</font>
+		<section class="contact-header">
+			<h3>Didnt find the right tool for the job, or just wanted to say how aswesome the site is?</h3>
+			<h3>Send us a message, Peace!</h3>
+		</section>
+		<form method="post" id="contactus_form">
+			Your Name:<input type="text" name="yourname" id="yourname" rows="1" value="" />
+			<br /><br />
+			Your Email:<input type="text" name="email" id="email" rows="1" value="" />
+			<br /><br />
+			Subject:<input type="text" name="subject" id="subject" rows="1" value=""></p>
+			<br /><br />
+			Leave a Message:<textarea name="message" id="message" ></textarea>
+			<br /><br />
+			<input type="submit" name="submit" id="submit" value="Send"/>
+		</form>
+	</div><!-- #content -->
+</div><!-- #primary -->
 <?php
 get_footer();
 ?>
---
+++ wp-content/themes/storefront-child/home.php
@@ -0,0 +1,4 @@
+<?php
+/*
+Template name: Home-page template
+*/
