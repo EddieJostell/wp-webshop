20761e317f153aaf6d38cfe18d2d5eb2a8716324
Eddie Jostell <eddie.jostell@gmail.com>
2017-05-22 17:20:24 +0200

    custom templates and woocommerce shop products

--- wp-content/themes/storefront-child/contact.php
+++ wp-content/themes/storefront-child/contact.php
@@ -3,77 +3,28 @@
 Template name: Contact Template
 
 */
-get_header(); ?>
-<div id="primary">
-	<div id="content" role="main">
-		<font color="#FF0000">
-			<?php
-			if(isset($_POST['submit']))
-			{
-				$flag=1;
-				if($_POST['yourname']=='')
-				{
-					$flag=0;
-					echo "Please Enter Your Name<br>";
-				}
-				else if(!preg_match('/[a-zA-Z_x7f-xff][a-zA-Z0-9_x7f-xff]*/',$_POST['yourname']))
-				{
-					$flag=0;
-					echo "Please Enter Valid Name<br>";
-				}
-				if($_POST['email']=='')
-				{
-					$flag=0;
-					echo "Please Enter E-mail<br>";
-				}
-				else if(!eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $_POST['email']))
-				{
-					$flag=0;
-					echo "Please Enter Valid E-Mail<br>";
-				}
-				if($_POST['subject']=='')
-				{
-					$flag=0;
-					echo "Please Enter Subject<br>";
-				}
-				if($_POST['message']=='')
-				{
-					$flag=0;
-					echo "Please Enter Message";
-				}
-				if ( empty($_POST) )
-				{
-					print 'Sorry, your nonce did not verify.';
-					exit;
-				}
-				else
-				{
-					if($flag==1)
-					{
-						wp_mail(get_option("admin_email"),trim($_POST[yourname])." sent you a message from ".get_option("blogname"),stripslashes(trim($_POST[message])),"From: ".trim($_POST[yourname])." <".trim($_POST[email]).">rnReply-To:".trim($_POST[email]));
-						echo "Mail Successfully Sent";
-					}
-				}
-			}
-			?>
-		</font>
-		<section class="contact-container">
-			<h3>Didnt find the right tool for the job, or just wanted to say how aswesome the site is?</h3>
-			<h3>Send us a message, Peace!</h3>
-			<form method="post" id="contactus_form">
-				Your Name:<input type="text" name="yourname" id="yourname" rows="1" value="" />
-				<br /><br />
-				Your Email:<input type="text" name="email" id="email" rows="1" value="" />
-				<br /><br />
-				Subject:<input type="text" name="subject" id="subject" rows="1" value=""></p>
-				<br /><br />
-				Leave a Message:<textarea name="message" id="message" ></textarea>
-				<br /><br />
-				<input type="submit" name="submit" id="submit" value="Send"/>
-			</form>
-		</section>
-	</div><!-- #content -->
-</div><!-- #primary -->
+get_header();
+ ?>
+ <div id="primary">
+		<main id="main" role="main">
+
+			<?php while ( have_posts() ) : the_post();
+
+				do_action( 'storefront_page_before' );
+
+				get_template_part( 'content', 'page' );
+
+				/**
+				 * Functions hooked in to storefront_page_after action
+				 *
+				 * @hooked storefront_display_comments - 10
+				 */
+				do_action( 'storefront_page_after' );
+
+			endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 <?php
 get_footer();
 ?>
--- wp-content/themes/storefront-child/home.php
+++ wp-content/themes/storefront-child/home.php
@@ -2,3 +2,43 @@
 /*
 Template name: Home-page template
 */
+
+get_header();
+?>
+<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post();
+
+				do_action( 'storefront_page_before' );
+
+				get_template_part( 'content', 'page' );
+
+				/**
+				 * Functions hooked in to storefront_page_after action
+				 *
+				 * @hooked storefront_display_comments - 10
+				 */
+				do_action( 'storefront_page_after' );
+
+			endwhile; // End of the loop. ?>
+				<section class="home-page">
+					<h2>Recent Posts</h2>
+					<?php
+					$args = array("posts_per_page" => 2, "orderby" => "comment_count");
+					$posts_array = get_posts($args);
+					foreach($posts_array as $post)
+					{
+						echo "<h1>" . $post->post_title . "</h1><br>";
+						echo "<p>" . $post->post_content . "</p><br>";
+						echo "<p>" . $post->post_date . "</p><br>";
+					} 
+					?>
+				</ul>  
+			</section>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php
+
+	get_footer();
+	?>
